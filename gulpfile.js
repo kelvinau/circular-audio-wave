3323b691caf9dce1cbfd1ef92c2f46a4b12e322c
Kelvin Au <kelvinau4413@hotmail.com>
2018-05-29 22:23:35 -0700

    add babel for ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 const gulp = require('gulp');
-const composer = require('gulp-uglify/composer');
-const uglify = require('gulp-uglify-es').default;
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 const pump = require('pump');
 const concat = require('gulp-concat');
- 
-gulp.task('compress',  cb => {
-  pump([
-        gulp.src(['lib/*.js', 'src/js/*.js']),
-        concat('circular-audio-wave.min.js'),
-        uglify(),
-        gulp.dest('dist')
-    ],
-    cb
-  );
+
+gulp.task('build', cb => {
+	pump([
+			gulp.src(['lib/*.js', 'src/js/*.js']),
+			babel({
+				ignore: ['lib/*.js'],
+				presets: ['env']
+			}),
+			concat('circular-audio-wave.min.js'),
+			uglify(),
+			gulp.dest('dist')
+		],
+		cb
+	);
 });
