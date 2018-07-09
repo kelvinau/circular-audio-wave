306f295054a2d11531de4d5557cec76987d161bc
Kelvin Au <kelvinau4413@hotmail.com>
2018-07-08 19:46:14 -0700

    remove babel and simply use gulp-uglify-es

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
 const gulp = require('gulp');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
+const uglify = require('gulp-uglify-es').default;
 const pump = require('pump');
 const concat = require('gulp-concat');
 
 gulp.task('build', cb => {
 	pump([
 			gulp.src(['lib/*.js', 'src/js/*.js']),
-			babel({
-				ignore: ['lib/*.js'],
-				presets: ['env']
-			}),
 			concat('circular-audio-wave.min.js'),
 			uglify(),
 			gulp.dest('dist')
